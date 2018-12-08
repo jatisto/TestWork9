a88d7fae2377ece8ebf177d162558c65f250e346
jatisto <j.atisto@gmail.com>
2018-12-08 11:47:52 +0600

    AddPropertyCode

--- TestWork9/TestWork9/Models/ApplicationUser.cs
+++ TestWork9/TestWork9/Models/ApplicationUser.cs
@@ -9,6 +9,6 @@ namespace TestWork9.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-       // public string Code { get; set; }
+       public string Code { get; set; }
     }
 }
